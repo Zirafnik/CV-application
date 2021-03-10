57dec5a95023c976b01193cbe253e74a4e479b4b
Zirafnik <dh@ptica.com>
2021-03-10 12:40:14 +0100

    general module style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import General from './components/General';
+import './styles/reset.css'
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>CV Application with React</h1>
+      <h1 className="h1">CV Application with React</h1>
       <General />
     </div>
   );
--- src/components/General.js
+++ src/components/General.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Text from './Text';
+import styles from '../styles/General.module.css';
 
 class General extends Component {
     constructor(props) {
@@ -44,27 +45,31 @@ class General extends Component {
     render() {
         if(this.state.saved===false) {
             return (
-                <form className='form'>
-                    <h2>General Information</h2>
-                    <label>First Name:
-                        <input name='firstName' onChange={this.handleChange} value={this.state.firstName} type="text" placeholder="First Name" required></input>
-                    </label>
-                    <label>Last Name:
-                        <input name='lastName' onChange={this.handleChange} value={this.state.lastName} type="text" placeholder="Last Name" required></input>
-                    </label>
-                    <label>Email:
-                        <input name='email' onChange={this.handleChange} value={this.state.email} type="email" placeholder="somebody@example.com" required></input>
-                    </label>
-                    <label>Phone Number:
-                        <input name='phone' onChange={this.handleChange} value={this.state.phone} type="text" placeholder="123-456-789" required></input>
-                    </label>
+                <form className={styles.container}>
+                    <h2 className={styles.h1}>General Information</h2>
+                    <div className={styles.wrapper}>
+                        <label>First Name:
+                            <input name='firstName' onChange={this.handleChange} value={this.state.firstName} type="text" placeholder="First Name" required></input>
+                        </label>
+                        <label>Last Name:
+                            <input name='lastName' onChange={this.handleChange} value={this.state.lastName} type="text" placeholder="Last Name" required></input>
+                        </label>
+                    </div>
+                    <div className={styles.wrapper}>
+                        <label>Email:
+                            <input name='email' onChange={this.handleChange} value={this.state.email} type="email" placeholder="somebody@example.com" required></input>
+                        </label>
+                        <label>Phone Number:
+                            <input name='phone' onChange={this.handleChange} value={this.state.phone} type="text" placeholder="123-456-789" required></input>
+                        </label>
+                    </div>
 
                     <button onClick={this.handleSave}>{this.state.saveName}</button>
                 </form>
             );
         } else {
             return (
-                <div>
+                <div className={styles.container}>
                     <h2>General Information</h2>
                     <Text state={this.state} />
                     <button onClick={this.handleSave}>{this.state.saveName}</button>
