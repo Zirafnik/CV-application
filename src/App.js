26bb7688c2c19e82dde8d428f1f5d7feff03e479
Zirafnik <dh@ptica.com>
2021-03-11 10:39:48 +0100

    added add button for adding new entries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+import React, {Component} from 'react';
+import uniqid from 'uniqid';
 import General from './components/General';
 import './styles/reset.css'
 import './styles/App.css';
+import Education from './components/Education';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="h1">CV Application with React</h1>
-      <General />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      educationID: [],
+      experienceID: [],
+    }
+
+    this.addEducation= this.addEducation.bind(this);
+    this.showEducation= this.showEducation.bind(this);
+  }
+
+  addEducation() {
+    this.setState({
+      educationID: this.state.educationID.concat(uniqid()),
+    })
+  }
+
+  showEducation() {
+    return(
+      this.state.educationID.map((id) =>
+        <Education key={id} />
+      )
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="h1">CV Application with React</h1>
+
+        <General />
+
+        <div>
+          {this.showEducation()}
+          <button onClick={this.addEducation}>Add</button>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
